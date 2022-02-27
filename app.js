e269bf120d534d348058917879294189bb80331a
st3nn3ts <100172200+st3nn3ts@users.noreply.github.com>
2022-02-27 18:21:07 +0000

    Update app.js
    27 /2-22,mousover and mouse out added.Deleted all testt-line above . Teacher like  consistence:)

--- app.js
+++ app.js
@@ -1,141 +1,33 @@
-/*
-// FUNCTIONAL GAME !
-window.addEventListener('DOMContentLoaded', (event) => {
-   console.log('DOM fully loaded and parsed');
+      // Soon finish..needd computer random move ++
 
-/*
-// const  mousetable = document.querySelectorAll('.box');
-   const mousetable = Array.from(document.getElementsByClassName('box'));
-         //   MouseOver & mouseOut for all spot's
-   mousetable.forEach(box => {box.addEventListener('mouseover',myfunction1)});
-   function myfunction1 () {
-     this.classList.add('boxHuman')
-     console.log('mouseover')//ok
-}
-  // if I use  /**\ (remove mouseout),then mouseover work
-   mousetable.forEach(box => {box.addEventListener('mouseout',myfunction2)});
-   function myfunction2 () {
-   this.classList.remove('boxHuman')
-   console.log('mouseout')
-console.log('mouseover')//ok
-}
-*/
-    /*      //my game setUp
-const table = Array.from(document.getElementsByClassName('box'));
-      //struggeling a bit to get safe random pc move
-//let free = () => table[i].filter(box => box.innerHTML === "");
-//let free = table.filter()box => {box.classList.contains('boxHuman','boxComputer')})
-//const pcMove = free[Math.floor(Math.random()*free.length)];
-
-let infoTxt = document.querySelector('h1');
-let currenP = 'o';
-let played = []  //no need
-let table2 = new Array(9).fill(null);
-//let isLeft = [1,2,3,4,5,6,7,8,9];
-   const winningCombo =
-     [
-     [1,4,7],
-     [2,5,8],
-     [0,1,2],
-     [2,4,6],
-     [0,4,8],
-     [3,4,5],
-     [6,7,8],
-     [0,3,6]
- ];
-   //  GAME START
-function myfunction(e){
-
- const boxArr = Array.from(document.getElementsByClassName('box'));
-const index = boxArr.indexOf(e.target);
-const free = boxArr.filter(box => box.innerHTML === "");
-const pcmove = free[Math.floor(Math.random()*free.length)].classList.add('boxComputer')
-//let pcMove = free[Math.floor(Math.random()*free.length)];
-table2[index] = currenP;
-played.push(1)
-if (currenP === 'o') {
-  this.classList.add('boxHuman')
-infoTxt.innerHTML = 'pc\'s thinking...',infoTxt.style.color = 'withe'
-} else {
-  currenP  = 'x'
-//  setTimeout(() {
-  table[i].classList.add('boxComputer')
-//free[Math.floor(Math.random()*free.length)].classList.add('boxComputer')
-//played.push(1) //Give extra push's (Error)
- infoTxt.innerHTML = 'Human\'s turn'
-}
-//},1000);
-if (winner()){
- alert(currenP + '  won. Restart ?')
-restart()
-}
-//check for Draw
-//if(!bord.some((e) => e === null)) alert('draw',Restart ?) restart()
-if(played.length == 9 ){
-alert('It\'s a Draw..Re-Mach ?')
-restart()
-}
-
-currenP = currenP == 'o' ? 'x' : 'o';
-}
-//winning loop .  If some roww (1,2,3) *winnerOptiens.length is trur return ===  true.
-const winner = () => {
- return winningCombo.some((combination) => {
-   if(
-currenP == table2[combination[0]]  &&
-   table2[combination[0]] == table2[combination[1]] &&
-   table2[combination[0]] == table2[combination[2]]
-)
-return true;
-console.log('check winn   '+ '  playd '+played + 'table2  ' + table2)
-return false;
-});
-}
-
-function restart (){ //table.forEach(el => {el.classList.remove('boxC','boxH')});
-for( i= 0; i< table.length; i++){
-table[i].classList.remove('boxComputer','boxHuman')
-infoTxt.innerText = 'NEW GAME '
-currenP = 'o'
-played.length = '';
-table2.fill(null);
-table[i].innerHTML = ''
-table.forEach(box => {box.addEventListener('click',myfunction,{once:true})});
-
-console.log('restart')
-}}
-//const on = () => {
-table.forEach(box => {box.addEventListener('click',myfunction,{once:true})});
-});
-//const off = () => {
-//table.forEach(box => {box.removeEventListener('click',myfunction)});} // Exprimential stuff
-
-//  TASK :1 add setTimeout.2 why isn't mouse function's work? Random spot work's  only'randomly' ?
-    ____---------------_____________-----------------__________________----------------____---\\
-
-
-// FUNCTIONAL GAME !
-window.addEventListener('DOMContentLoaded', (event) => {
-   console.log('DOM fully loaded and parsed');
-/*
-
-  // const  mousetable = document.querySelectorAll('.box');
-  const mousetable = Array.from(document.getElementsByClassName('box'));
-         //   MouseOver & mouseOut for all spot's
-   mousetable.forEach(box => {box.addEventListener('mouseover',myfunction1)});
-   function myfunction1 () {
-  this.classList.add('boxH')
-
-   }
-
-    */     // Soon finish..needd computer random move
-    window.addEventListener('DOMContentLoaded', (event) => {
+     window.addEventListener('DOMContentLoaded', (event) => {
        console.log('DOM fully loaded and parsed');
+   
+//The name mouse'tables is for mouseFunctions ONLY.It Make it easy'r to undestand/read code,
+// ..and wee bee sure too avoid table-name-conflict's futher down.
+
+       const mousetable = document.querySelectorAll('li')//NodeList
+        //Event listener 'mouseover'  for all box's
+  mousetable.forEach(box => {box.addEventListener('mouseover',mouseOver)})
+// show  boxHuman on 'this'...('this' is the  present  box pointed at by human player)
+function  mouseOver () {
+       this.classList.add('boxHuman')
+       console.log('mouseover');
+}
+//Event Listener mouseOut  (to remove 'boxHuman')
+mousetable.forEach(box => {box.addEventListener('mouseout',mouseOut)})
+//mouseout now remove 'boxHuman' from 'this' box,and give us the hoover effect).
+function mouseOut () {
+          this.classList.remove('boxHuman')
+         console.log('mouseout');
+ }
+     
+//Got table(Array)from HTML page to play on.       
 const table = Array.from(document.getElementsByClassName('box'));
 
 let infoTxt = document.querySelector('h1');
 let currenP = 'o';
-let played = []  //no need
+let played = []  
 let table2 = new Array(9).fill(null);
 //let isLeft = [1,2,3,4,5,6,7,8,9];
    const winnOptions =
